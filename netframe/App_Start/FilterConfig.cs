83646c43abab3690ce727df3812822a10b875d07
DESKTOP-QSEF0D3\大哥 <carl12331@gmail.com>
2022-06-19 23:59:09 +0800

    netcore

---
+++ netframe/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace netframe
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ netframe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="netframe.MvcApplication" Language="C#" %>
