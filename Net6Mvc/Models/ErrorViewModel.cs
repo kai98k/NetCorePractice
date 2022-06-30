0161a9d81e9631e975f6bc1848330c6c416f2779
L1uXO330B7 <carl12331.gmail.com>
2022-06-30 15:03:08 +0800

    UPDATE

--- Net6Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Net6Mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
